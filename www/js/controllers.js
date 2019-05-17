5bc6479bb4f9973aa16352668ea70bf4335f9a5a
walter21gt <fabriciogt009@gmail.com>
2019-05-17 16:21:40 -0500

    version final

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -23,6 +23,134 @@ angular.module('starter.controllers', [])
 })
 
 .controller('AccountCtrl', function($scope) {
+  /* co-created with Rémi -> https://github.com/strowbeary
+ thank you */
+
+class SineCanvas{
+    constructor() {
+        this.canvas = document.querySelector('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.drawers = [];
+
+        this.addEventListener();
+    }
+
+    update() {
+        this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
+        for (let child of this.drawers) {
+            child.update();
+        }
+        requestAnimationFrame(() => this.update())
+    }
+
+    onResize() {
+        this.canvas.width = window.innerWidth = 600;
+        this.canvas.height = window.innerHeight;
+        for (let drawer of this.drawers) (
+            drawer.init()
+        )
+    }
+
+    addChild(child) {
+        this.drawers.push(child);
+        this.onResize();
+        this.update();
+    }
+
+    addEventListener() {
+        window.addEventListener('resize', () => this.onResize());
+    }
+}
+
+class SineWave {
+    constructor(options, canvas) {
+        this.options = {
+            pointSpacing: 10,
+            amplitude: 55,
+            lineNumber: 6,
+            particuleAlpha: 0.8,
+            dx: 0.20, //taille d'une periode
+            lineSpacing: 20,
+            theta: 0.02, //vitesse
+            periodOffset: 0,
+            dotColor: "white",
+            maxDotSize: 3,
+            minDotSize: 0.1
+        };
+        this.curveOffset = 0;
+        this.canvas = canvas;
+        this.options = Object.assign(this.options, options);
+        this.points = [];
+        this.pointsZ = [];
+    }
+
+    init() {
+        let canvasHeight = this.canvas.canvas.height;
+        this.points = Array(Math.floor(canvasHeight / this.options.pointSpacing))
+            .fill(0, 0, Math.floor(canvasHeight / this.options.pointSpacing) - 1);
+        this.pointsZ = Array(Math.floor(canvasHeight / this.options.pointSpacing))
+            .fill(0, 0, Math.floor(canvasHeight / this.options.pointSpacing) - 1);
+    }
+
+    update(){
+        this.curveOffset += this.options.theta;
+        let x = this.curveOffset;
+        for(let i = 0; i < this.points.length; i++) {
+            this.points[i] = Math.sin(x + this.options.periodOffset) * this.options.amplitude;  // Math.log(i / 50);
+            this.pointsZ[i] = (Math.sin(x + this.options.periodOffset + Math.PI / 2) + 1 + this.options.minDotSize)/ 2;
+            x += this.options.dx;
+        }
+        this.draw();
+    }
+
+    draw(){
+        let canvasWidth = this.canvas.canvas.width;
+        let ctx = this.canvas.ctx;
+
+        ctx.fillStyle = this.options.dotColor;
+        for (let i = 0; i < this.options.lineNumber; i++) {
+            for (let n = 0; n < this.points.length; n++) {
+                ctx.beginPath();
+                ctx.ellipse(
+                    (canvasWidth - (this.options.lineNumber * this.options.pointSpacing / 2)) / 2 + this.points[n] - i * 10,
+                    n * this.options.lineSpacing - i * 10,
+                    this.options.maxDotSize * this.pointsZ[n],
+                    this.options.maxDotSize * this.pointsZ[n],
+                    0,
+                    0,
+                    2 * Math.PI
+                );
+                ctx.closePath();
+                ctx.fill();
+            }
+        }
+    }
+}
+
+
+let sineCanvas = new SineCanvas();
+
+sineCanvas.addChild(
+    new SineWave({
+        lineNumber: 1,
+        amplitude: 55,
+        theta: 0.035,
+        maxDotSize: 2.5,
+        minDotSize: 0.4
+    }, sineCanvas)
+);
+
+sineCanvas.addChild(
+    new SineWave({
+        periodOffset: Math.PI,
+        lineNumber: 1,
+        amplitude: 55,
+        theta: 0.035,
+        maxDotSize: 2.5,
+        minDotSize: 0.4
+    }, sineCanvas)
+);
+  
   $scope.settings = {
     enableFriends: true
   };
--- www/js/services.js
+++ www/js/services.js
@@ -6,27 +6,28 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    name: 'Metodos Anticonceptivos',
+    lastText: 'Son los métodos o procedimientos que previenen un embarazo en mujeres sexualmente activas, ya sean ellas o sus parejas quienes los usen. Pueden ser hormonales o no hormonales, transitorios o definitivos, basados en tecnología o en conductas',
+    face: 'img/imagen1.jpg'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    name: 'Metodos Anticonceptivos',
+    lastText: 'Métodos combinados que contienen estrógeno y progestágeno y que se pueden administrar como píldoras, inyecciones, anillo vaginal o parche transdérmico. Métodos que contienen solo progestágeno y que se pueden administrar como píldoras, inyecciones, implantes, anillo vaginal o dispositivo intrauterino.',
+    face: 'img/imagen2.jpg'
+    
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
+    name: 'Metodos Anticonceptivos',
     lastText: 'I should buy a boat',
     face: 'img/adam.jpg'
   }, {
     id: 3,
-    name: 'Perry Governor',
+    name: 'Metodos Anticonceptivos',
     lastText: 'Look at my mukluks!',
     face: 'img/perry.png'
   }, {
     id: 4,
-    name: 'Mike Harrington',
+    name: 'Metodos Anticonceptivos',
     lastText: 'This is wicked good ice cream.',
     face: 'img/mike.png'
   }];
